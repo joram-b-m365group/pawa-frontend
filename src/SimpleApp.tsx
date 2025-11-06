4a1e6752c7068d10d7d836a65b2e033492fde4f0
joram-b-m365group <joram.b@m365connect.com>
2025-11-06 11:17:09 +0300

    Use simpler app layout - EnhancedChatInterface directly

---
+++ src/SimpleApp.tsx
@@ -0,0 +1,11 @@
+import EnhancedChatInterface from './components/EnhancedChatInterface'
+
+function SimpleApp() {
+  return (
+    <div className="w-full h-screen bg-gray-900">
+      <EnhancedChatInterface />
+    </div>
+  )
+}
+
+export default SimpleApp
